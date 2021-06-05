f97e560b810d55f3d9132a04510fe78d4dd144ca
“pavel.zagalsky” <zagalsky@gmail.com>
2021-06-05 23:57:34 +0300

    added an endpoint for tests

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, Response
+import json
 app = Flask(__name__)
+JSON_RESPONSE = {
+    "message": "all good"
+}
 
 
 @app.route("/")
 def hello():
     return "Hello from Python!"
+
+
+@app.route("/json")
+def json():
+    return Response(json.dumps(JSON_RESPONSE), status=200, mimetype='application/json')
 
 
 if __name__ == "__main__":
